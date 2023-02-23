77a9ffc3395465d41dd3906ea1929f24d06bc125
jewelrana3 <juyelrana7752@gmail.com>
2023-02-23 08:55:30 +0600

    jaja

--- json.js
+++ json.js
@@ -1,4 +1,21 @@
-const user = {id:101, name:'juyel', job:'devoloper'}
-const stringlyfyed = JSON.stringify(user)
-console.log(user)
-console.log(stringlyfyed)
+// const user = {id:101, name:'juyel', job:'devoloper'}
+// const stringlyfyed = JSON.stringify(user)
+// console.log(user)
+// console.log(stringlyfyed)
+
+const user = {
+    name:'mama',
+    location:'dhaka',
+    product:{
+        tv:'led',
+        wage:'manik',
+
+    },
+    point: ['laptop','moblie','tv']
+}
+
+console.log(user);
+const userSide = JSON.stringify(user);
+console.log(userSide);
+const parseObject = JSON.parse(userSide);
+console.log(parseObject)
